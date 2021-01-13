dcf4e81ef5d3895e6933a41aeceeb16aa287d976
DESKTOP-6PP8ISQ\TOSHIBA <jrdeleon@csmbataan.ph.education>
2021-01-13 21:18:03 +0800

    Creating file and adding text box,label and text block adding some limit in txtmessage

---
+++ Daniel.Texting API/Daniel.Texting API/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Daniel.Texting_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtmessage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            charCount();
+            txtmessage.MaxLength = 100;
+        }
+
+        private void charCount()
+        {
+            int j = txtmessage.Text.Length;
+            string charcount = j.ToString() + " " + "/" + " " + "100";
+            CharCount.Text = charcount;
+        }
+    }
+}
